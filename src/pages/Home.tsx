593b9f6a255e4f4d7186c789ba59e43b307c1111
Michael Wood <michaelwood33311@icloud.com>
2023-12-12 09:09:34 -0500

    added submit button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonInput, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import NewsArticles from '../'
 import './Home.scss';
@@ -61,6 +61,7 @@ const Home: React.FC = () => {
             </div>
          </div>
          <div className='Home__curnum'>{curNum}</div>
+         <IonButton className='Home__submit'>Submit</IonButton>
             
         </div>
       </IonContent>
