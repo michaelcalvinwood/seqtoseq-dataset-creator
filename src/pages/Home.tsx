6c0e7bb978f8b455b82afa0a2720e68ecf57685a
Michael Wood <michaelwood33311@icloud.com>
2023-12-12 08:37:03 -0500

    added input selector

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import NewsArticles from '../'
+import './Home.scss';
+import { useState } from 'react';
+
 
 const Home: React.FC = () => {
+  const [input, setInput] = useState('NewsArticles')
+
+  const processNewsArticles = () => {
+
+  }
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'NewsArticles.csv',
+      value: 'NewsArticles',
+      url: 'https://michaelcalvinwood.net/datasets/text-data/NewsArticles.csv',
+      processor: processNewsArticles
+    }
+  ]
+
   return (
-    <IonPage>
+    <IonPage className='Home'>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle className='Home__title'>Seq2Seq Data Set</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className='Home__content'>
+          <IonSelect label="Input:" value={input} onIonChange={(e) => setInput(e.detail.value)}>
+            {inputs.map(input => {
+              return (
+                <IonSelectOption key={input.id} value={input.value}>{input.name}</IonSelectOption>
+              )
+            })
+
+            }
+            </IonSelect>
+        </div>
       </IonContent>
     </IonPage>
   );
