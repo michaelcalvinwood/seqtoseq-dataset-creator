d6e943377a2cbc652d27090120d89d83c488d926
Michael Wood <michaelwood33311@icloud.com>
2023-12-12 09:07:58 -0500

    added display current offset

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,8 @@ const Home: React.FC = () => {
   const [input, setInput] = useState(inputs[0].value)
   const [output, setOutput] = useState(inputs[0].name)
   const [start, setStart] = useState('0')
-  const [tokens, setTokens] = useState('400')
+  const [tokens, setTokens] = useState('400');
+  const [curNum, setCurNum] = useState(Math.floor(Number(start)))
 
 
   return (
@@ -36,22 +37,30 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen style={{backgroundColor: 'var(ion-color-secondary) !important'}}>
         <div className='Home__content'>
+         <div className="Home__settings">
           <div className="Home__input-container">
-            <IonSelect className='Home__input' label="Input:" value={input} onIonChange={(e) => setInput(e.detail.value)}>
-              {inputs.map(input => {
-                return (
-                  <IonSelectOption key={input.id} value={input.value}>{input.name}</IonSelectOption>
-                )
-              })
+              <IonSelect className='Home__input' label="Input:" value={input} onIonChange={(e) => setInput(e.detail.value)}>
+                {inputs.map(input => {
+                  return (
+                    <IonSelectOption key={input.id} value={input.value}>{input.name}</IonSelectOption>
+                  )
+                })
 
-              }
-              </IonSelect>
-              <IonInput className='Home__start' label="Start:" type="number" min="0" value={start} onIonInput={(e) => setStart(e.detail.value) }/>
-          </div>
-          <div className="Home__output-container">
-            <IonInput className='Home__output' label="Output:" value={output} onIonInput={(e) => setOutput(e.detail.value)}/>
-            <IonInput className='Home__tokens' label="Tokens:" type="number" min="100" value={tokens} onIonInput={(e) => setTokens(e.detail.value) }/>
-          </div>
+                }
+                </IonSelect>
+                <IonInput className='Home__start' label="Start:" type="number" min="0" value={start} 
+                  onIonInput={(e) =>  {
+                    setStart(e.detail.value)
+                    setCurNum(Math.floor(Number(e.detail.value)))
+                  } }
+                />
+            </div>
+            <div className="Home__output-container">
+              <IonInput className='Home__output' label="Output:" value={output} onIonInput={(e) => setOutput(e.detail.value)}/>
+              <IonInput className='Home__tokens' label="Tokens:" type="number" min="100" value={tokens} onIonInput={(e) => setTokens(e.detail.value) }/>
+            </div>
+         </div>
+         <div className='Home__curnum'>{curNum}</div>
             
         </div>
       </IonContent>
